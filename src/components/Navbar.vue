aeacb388b46b1127b4cf8940ffb8d59899d6f69e
Nikola <milnikola81@gmail.com>
2018-08-24 21:09:04 +0200

    finished no.1

---
+++ src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <nav class="navbar navbar-dark bg-dark">
+            <a class="navbar-brand" href="#">TriviaApp</a>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
---
+++ src/components/TriviaApp.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>trivia app</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
